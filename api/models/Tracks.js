42c07b050eb2595f1ad863e35a8006d1b983ea55
omar <omar.karaksi09@gmail.com>
2016-01-23 22:44:06 +0200

    added files

---
+++ api/models/Tracks.js
@@ -0,0 +1,19 @@
+/**
+* Tracks.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+  //connection :'redis',	
+  attributes: {
+  	'track_title':'STRING',
+  	'track_mp3_url':'STRING',
+  	'track_length':'FLOAT',
+  	'owner':{
+  		model: 'podcasts'	
+  	}
+  }
+};
+
---
+++ config/connections.js
@@ -0,0 +1,11 @@
+module.exports.connections = {
+	redis :{
+		adapter		: 'sails-redis',
+		host		: 'pub-redis-17907.us-east-1-2.1.ec2.garantiadata.com',
+		password	: ')neverforgetyourpassword(',		
+		port		:  17907,
+		no_ready_check :true 
+
+	}
+
+}
